0445adb01302dba95b5e7b1538ffbe3ba9961ca7
gelis07 <baggelispoz@gmail.com>
2023-08-04 00:33:55 +0300

    Moved the CMake files from the bin folder

---
+++ CMakeFiles/Sapphire-Engine.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx.cxx" "CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx.gch" "gcc" "CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx.gch.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx.gch" "gcc" "CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx.gch.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/ImGuizmo.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/ImGuizmo.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/ImGuizmo.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/ImGuizmo.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/ImGuizmo.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui_draw.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_draw.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_draw.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui_impl_glfw.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_glfw.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_glfw.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui_impl_opengl3.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_opengl3.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_impl_opengl3.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui_stdlib.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_stdlib.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_stdlib.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_stdlib.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_stdlib.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui_tables.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_tables.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_tables.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/include/Imgui/imgui_widgets.cpp" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_widgets.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/include/Imgui/imgui_widgets.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Engine/Engine.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Engine.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Engine.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Engine.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Engine.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Engine/Scenes.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Scenes.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Scenes.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Scenes.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Scenes.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Engine/Views/PlayMode.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/PlayMode.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/PlayMode.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/PlayMode.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/PlayMode.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Engine/Views/SceneEditor.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/SceneEditor.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/SceneEditor.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/SceneEditor.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Engine/Views/SceneEditor.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Graphics/Grid.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Grid.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Grid.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Grid.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Grid.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Graphics/Shapes.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Shapes.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Shapes.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Shapes.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Graphics/Shapes.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Objects/Objects.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Objects/Objects.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Objects/Objects.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Objects/Objects.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Objects/Objects.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Scripting/Components.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Components.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Components.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Components.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Components.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Scripting/LuaUtilities.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/LuaUtilities.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/LuaUtilities.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/LuaUtilities.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/LuaUtilities.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Scripting/Scripting.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Scripting.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Scripting.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Scripting.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Scripting.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/Scripting/Variables.cpp" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Variables.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Variables.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Variables.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/Scripting/Variables.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/UI/FileExplorer/File.cpp" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/File.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/File.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/File.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/File.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/UI/FileExplorer/FileExplorer.cpp" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/FileExplorer.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/FileExplorer.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/FileExplorer.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/FileExplorer/FileExplorer.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/UI/ProjectManager.cpp" "CMakeFiles/Sapphire-Engine.dir/src/UI/ProjectManager.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/ProjectManager.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/UI/ProjectManager.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/ProjectManager.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/UI/Windows.cpp" "CMakeFiles/Sapphire-Engine.dir/src/UI/Windows.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/Windows.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/UI/Windows.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/UI/Windows.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/main.cpp" "CMakeFiles/Sapphire-Engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/main.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/main.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/src/stb_image.cpp" "CMakeFiles/Sapphire-Engine.dir/src/stb_image.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/stb_image.cpp.obj.d"
+  "D:/Gelis/Programs/Sapphire-Engine/CMakeFiles/Sapphire-Engine.dir/cmake_pch.hxx" "CMakeFiles/Sapphire-Engine.dir/src/stb_image.cpp.obj" "gcc" "CMakeFiles/Sapphire-Engine.dir/src/stb_image.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
