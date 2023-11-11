35775e43d158c16448f8459582997262c568a8aa
Gelis <baggelispoz@gmail.com>
2023-11-12 00:51:27 +0200

    hi

---
+++ src/Editor/Views/PlayMode.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Utilities.hpp"
+#include "UI/Windows.h"
+#include "Engine/Scenes.h"
+#include "Graphics/Renderer/FrameBuffer.h"
+#include <GLFW/glfw3.h>
+class PlayMode{
+    public:
+        void Init(Scene* activeScene);
+        void Render(const std::string& MainPath);
+        bool GameRunning = false;
+        bool Paused = false;
+        std::shared_ptr<Object> CameraObject;
+    private:
+        SapphireRenderer::FrameBuffer FrameBuffer;
+        std::string m_SceneFileName;
+        int m_WindowWidth, m_WindowHeight;
+        Scene* m_ActiveScene;
+        GLFWwindow* m_Window;
+        bool m_Start = true;
+};
---
+++ src/Editor/Views/SceneEditor.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "UI/Windows.h"
+#include "Graphics/Grid.h"
+#include "Engine/Scenes.h"
+#include "Graphics/Renderer/FrameBuffer.h"
+#include <GLFW/glfw3.h>
+
+
+constexpr glm::vec4 BackgroundColor = glm::vec4(0,0,0,1);
+
+
+
+class SceneEditor{
+    public:
+        void Render();
+        void Init(Scene* activeScene);
+        ViewportCamera ViewCamera; //This Camera is only for the m_Viewport NOT the game's camera
+        std::shared_ptr<Object> SelectedObj;
+        glm::vec2 GetWindowSize() {return glm::vec2(m_WindowWidth, m_WindowHeight);}
+    private:
+        Grid m_Grid;
+        SapphireRenderer::FrameBuffer FrameBuffer;
+        ImGuiContext* m_ViewportContext;
+        Scene* m_ActiveScene;
+        int m_WindowWidth; 
+        int m_WindowHeight;
+        GLFWwindow* m_Window;
+        std::shared_ptr<Object> OnClick(GLFWwindow* window, std::vector<std::shared_ptr<Object>> Objects, glm::vec2&& WindowPosition); 
+        void MoveCamera(glm::vec2&& Size, glm::vec2&& Position);
+        static void Zooming(GLFWwindow* window, double xoffset, double yoffset);
+        bool m_ClickedOnObj;
+        bool m_FirstTime = true; // Indicates the first time the user clicks on the SelectedObj
+        glm::vec2 m_Offset; // Offset of the Cursor and the SelectedObj when the user first clicks on the SelectedObj
+
+        ImGuizmo::OPERATION m_Operation = ImGuizmo::OPERATION::TRANSLATE;
+};
